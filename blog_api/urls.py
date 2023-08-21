a195ca749117574ac2d42357752f2ee4a0a43268
thientantran <thientantm@gmail.com>
2023-08-21 14:38:25 +0700

    REST API
    pip install djangorestframework
    make serializer to convert data

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-# from .views import PostList, PostDetail
-from django.views.generic import TemplateView
+from .views import PostList, PostDetail
 app_name = 'blog_api'
 
 urlpatterns = [
-    # path('<int:pk>/', PostDetail.as_view(), name='detailcreate'),
-    # path('', PostList.as_view(), name='listcreate'),
-    path("", TemplateView.as_view(template_name="blog/index.html"))
+    path('<int:pk>/', PostDetail.as_view(), name='detailcreate'),
+    path('', PostList.as_view(), name='listcreate')
 ]
--- blog_api/views.py
+++ blog_api/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostList(generics.ListCreateAPIView):
+    # nhận được phương thức list hoặc create
+    queryset = Post.postobjects.all()
+    # collect data from database
+    serializer_class = PostSerializer
+    # serializer sẽ convert data of cái queryset và model thành data format của python,
+    # để chuyển về dạng json, dễ dàng khi response về frontend
+
+
+class PostDetail(generics.RetrieveDestroyAPIView):
+    # nhận được phương thức Retrive hoặc destroy
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+""" Concrete View Classes
+#CreateAPIView
+Used for create-only endpoints.
+#ListAPIView
+Used for read-only endpoints to represent a collection of model instances.
+#RetrieveAPIView
+Used for read-only endpoints to represent a single model instance.
+#DestroyAPIView
+Used for delete-only endpoints for a single model instance.
+#UpdateAPIView
+Used for update-only endpoints for a single model instance.
+##ListCreateAPIView
+Used for read-write endpoints to represent a collection of model instances.
+RetrieveUpdateAPIView
+Used for read or update endpoints to represent a single model instance.
+#RetrieveDestroyAPIView
+Used for read or delete endpoints to represent a single model instance.
+#RetrieveUpdateDestroyAPIView
+Used for read-write-delete endpoints to represent a single model instance.
+"""
