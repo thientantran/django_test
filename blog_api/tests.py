8f91aaea6a18a12ecd311dab53e3b49df375bb27
thientantran <thientantm@gmail.com>
2023-08-22 16:01:19 +0700

    Testing for Custom Permisson

--- blog_api/tests.py
+++ blog_api/tests.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from blog.models import Post, Category
 from django.contrib.auth.models import User
+from rest_framework.test import APIClient
 
 
 class PostTests(APITestCase):
@@ -24,13 +25,42 @@ class PostTests(APITestCase):
 
         self.testuser1 = User.objects.create_user(
             username='test_user1', password='123456789')
-
+        self.client.login(username=self.testuser1.username,
+                          password='123456789')
         data = {"title": "new", "author": 1,
                 "excerpt": "new", "content": "new"}
         url = reverse('blog_api:listcreate')
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(len(response.data), 6)
-        root = reverse(('blog_api:detailcreate'), kwargs={'pk': 1})
-        response = self.client.get(url, format='json')
+        # root = reverse(('blog_api:detailcreate'), kwargs={'pk': 1})
+        # response = self.client.get(url, format='json')
+        # self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_post_update(self):
+
+        client = APIClient()
+
+        self.test_category = Category.objects.create(name='django')
+        self.testuser1 = User.objects.create_user(
+            username='test_user1', password='123456789')
+        self.testuser2 = User.objects.create_user(
+            username='test_user2', password='123456789')
+        test_post = Post.objects.create(
+            category_id=1, title='Post Title', excerpt='Post Excerpt', content='Post Content', slug='post-title', author_id=1, status='published')
+        # test_post này mặc dù ko đc dùng ở dưới, nhưng mà chỗ này giống kiểu tạo ra data cho database
+        client.login(username=self.testuser1.username,
+                     password='123456789')
+        # nếu chỗ này mà login user2 sẽ bị sai
+
+        url = reverse(('blog_api:detailcreate'), kwargs={'pk': 1})
+
+        response = client.put(
+            url, {
+                "title": "New",
+                "author": 1,
+                "excerpt": "New",
+                "content": "New",
+                "status": "published"
+            }, format='json')
+        print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
