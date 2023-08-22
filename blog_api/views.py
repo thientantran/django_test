2accb1f5a1dfbbbb0201fb113ed2c6b18188a99a
thientantran <thientantm@gmail.com>
2023-08-22 15:36:06 +0700

    add Custom permission

--- blog_api/views.py
+++ blog_api/views.py
@@ -5,11 +5,25 @@ from .serializers import PostSerializer
 from rest_framework.permissions import SAFE_METHODS, IsAuthenticatedOrReadOnly, BasePermission, IsAdminUser, DjangoModelPermissions
 
 
+class PostUserWritePermission(BasePermission):
+    message = 'Editing posts is restricted to the author only.'
+
+    def has_object_permission(self, request, view, obj):
+        # chỗ này đặt là has_object_permission hoặc has_permission
+        # do cái này nó áp dụng cho ọbject nên phải có chữ object
+        #https://www.django-rest-framework.org/api-guide/permissions/#custom-permissions
+        if request.method in SAFE_METHODS:
+            # Check permissions for read-only request. Methods: 'GET', 'OPTIONS' and 'HEAD'
+            return True
+        else:
+            # Check permissions for write request
+            return obj.author == request.user
+        # mấy thằng là tác giả mới có thể xoá hoặc sửa, còn mấy thằng khác chỉ có thể get thôi
 class PostList(generics.ListCreateAPIView):
     # permission_classes = [IsAdminUser]
     permission_classes = [IsAuthenticatedOrReadOnly]
     # permission_classes = [DjangoModelPermissions]
-    # cái này là khi mình tạo user, có đưa vào 1 cái group và có add các permissions vào rồi, giờ nó tuân theo cái này  
+    # cái này là khi mình tạo user, có đưa vào 1 cái group và có add các permissions vào rồi, giờ nó tuân theo cái này
     # nhận được phương thức list hoặc create
     queryset = Post.postobjects.all()
     # collect data from database
@@ -18,7 +32,8 @@ class PostList(generics.ListCreateAPIView):
     # để chuyển về dạng json, dễ dàng khi response về frontend
 
 
-class PostDetail(generics.RetrieveDestroyAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView,PostUserWritePermission):
+    permission_classes = [PostUserWritePermission]
     # nhận được phương thức Retrieve hoặc destroy
     queryset = Post.objects.all()
     # cái phương thức này mặc định nó sẽ retrive và lấy theo Id
