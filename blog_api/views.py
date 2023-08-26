4e16369703a9ad8733cc94d2194e098c99b7f199
thientantran <thientantm@gmail.com>
2023-08-26 11:06:41 +0700

    Setup viewsets and routers for BlogAPI

--- blog_api/views.py
+++ blog_api/views.py
@@ -3,7 +3,10 @@ from blog.models import Post
 from .serializers import PostSerializer
 # add permission rules
 from rest_framework.permissions import SAFE_METHODS,IsAuthenticated, IsAuthenticatedOrReadOnly, BasePermission, IsAdminUser, DjangoModelPermissions
+from rest_framework import viewsets
 
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
 class PostUserWritePermission(BasePermission):
     message = 'Editing posts is restricted to the author only.'
@@ -19,26 +22,66 @@ class PostUserWritePermission(BasePermission):
             # Check permissions for write request
             return obj.author == request.user
         # mấy thằng là tác giả mới có thể xoá hoặc sửa, còn mấy thằng khác chỉ có thể get thôi
-class PostList(generics.ListCreateAPIView):
-    # permission_classes = [IsAdminUser]
-    # permission_classes = [IsAuthenticatedOrReadOnly]
+
+class PostList(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
-    # permission_classes = [DjangoModelPermissions]
-    # cái này là khi mình tạo user, có đưa vào 1 cái group và có add các permissions vào rồi, giờ nó tuân theo cái này
-    # nhận được phương thức list hoặc create
     queryset = Post.postobjects.all()
-    # collect data from database
-    serializer_class = PostSerializer
-    # serializer sẽ convert data of cái queryset và model thành data format của python,
-    # để chuyển về dạng json, dễ dàng khi response về frontend
+    # sử dụng viewset sẽ ko có các get, póst, mà sẽ dùng nhưng method list, retrieve được built sẵn để thực hiện
+    def list(self, request):
+        serializer_class = PostSerializer(self.queryset, many=True)
+        return Response(serializer_class.data)
+    def retrieve(self, request, pk=None):
+        post = get_object_or_404(self.queryset, pk=pk)
+        serializer_class = PostSerializer(post)
+        return Response(serializer_class.data)
 
+    # def list(self, request):
+    #     pass
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
-    permission_classes = [PostUserWritePermission]
-    # nhận được phương thức Retrieve hoặc destroy
-    queryset = Post.objects.all()
-    # cái phương thức này mặc định nó sẽ retrive và lấy theo Id
-    serializer_class = PostSerializer
+    # def create(self, request):
+    #     pass
+
+    # def retrieve(self, request, pk=None):
+    #     pass
+
+    # def update(self, request, pk=None):
+    #     pass
+
+    # def partial_update(self, request, pk=None):
+    #     pass
+
+    # def destroy(self, request, pk=None):
+    #     pass
+
+    # class PostList(viewsets.ModelViewSet):
+    #     permission_classes = [IsAuthenticated]
+    #     queryset = Post.postobjects.all()
+    #     serializer_class = PostSerializer
+
+    # class PostDetail(viewsets.ModelViewSet, PostUserWritePermission):
+    #     permission_classes = [PostUserWritePermission]
+    #     queryset = Post.objects.all()
+    #     serializer_class = PostSerializer
+# class PostList(generics.ListCreateAPIView):
+#     # permission_classes = [IsAdminUser]
+#     # permission_classes = [IsAuthenticatedOrReadOnly]
+#     permission_classes = [IsAuthenticated]
+#     # permission_classes = [DjangoModelPermissions]
+#     # cái này là khi mình tạo user, có đưa vào 1 cái group và có add các permissions vào rồi, giờ nó tuân theo cái này
+#     # nhận được phương thức list hoặc create
+#     queryset = Post.postobjects.all()
+#     # collect data from database
+#     serializer_class = PostSerializer
+#     # serializer sẽ convert data of cái queryset và model thành data format của python,
+#     # để chuyển về dạng json, dễ dàng khi response về frontend
+#
+#
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
+#     permission_classes = [PostUserWritePermission]
+#     # nhận được phương thức Retrieve hoặc destroy
+#     queryset = Post.objects.all()
+#     # cái phương thức này mặc định nó sẽ retrive và lấy theo Id
+#     serializer_class = PostSerializer
 
 """ Concrete View Classes
 #CreateAPIView
--- core/urls.py
+++ core/urls.py
@@ -23,11 +23,13 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('blog.urls', namespace='blog')),
-    path("api/", include('blog_api.urls', namespace='blog_api')),
-    path('api/user/', include('users.urls', namespace='users')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # bình thường thì chỉ có superuser (admin) có thể được login vào admin dashboard
-    # để các user khác có thể login vào rồi fetch API thì phải có cái api-auth
+    # đổi vị trí vì có thể nó sẽ nhận token như 1 key của post
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/user/', include('users.urls', namespace='users')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path("api/", include('blog_api.urls', namespace='blog_api')),
+    # bình thường thì chỉ có superuser (admin) có thể được login vào admin dashboard
+    # để các user khác có thể login vào rồi fetch API thì phải có cái api-auth
+
 ]
